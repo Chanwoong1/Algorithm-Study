319a265bb959e591357c9ab6825f627b4eac96fc
Chanwoong1 <jcw7468@gmail.com>
2022-10-18 11:18:57 +0900

    10/18 obj

--- baekjoon/silver/3tier/4375_1.py
+++ baekjoon/silver/3tier/4375_1.py
@@ -1,2 +1,21 @@
 # https://www.acmicpc.net/problem/4375
 
+from contextlib import nullcontext
+from doctest import BLANKLINE_MARKER
+
+
+while (1) :
+    try :
+        n = int(input())
+    except :
+        break
+    
+    num = 0
+    i = 1
+    while (1) :
+        num = num * 10 + 1
+        num %= n
+        if num == 0 :
+            print(i)
+            break
+        i += 1
