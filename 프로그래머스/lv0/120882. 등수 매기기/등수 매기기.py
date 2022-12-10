a6a963ed48093c630d4e184ba96dbb7833f8dc69
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2022-12-10 11:34:30 +0900

    [level 0] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -0,0 +1,7 @@
+def solution(score):
+    dict, avg_list = {}, [sum(num) / 2 for num in score]
+    for index, avg in enumerate(sorted(avg_list, reverse=True), start=1):
+        if avg not in dict:
+            dict[avg] = index
+
+    return [dict[avg] for avg in avg_list]
