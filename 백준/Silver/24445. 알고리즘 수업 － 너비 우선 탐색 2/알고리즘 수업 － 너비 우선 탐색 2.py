35746d6d7b9329e6703f18792adcd9fdbb8cef19
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2022-12-17 22:21:51 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 520 ms, Memory: 60624 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,27 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+N, M, R = map(int, input().split())
+visit = [0] * (N + 1)
+graph = [[] for _ in range(N + 1)]
+for _ in range(M) :
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+for i in range(N + 1) :
+    graph[i].sort(reverse = True)
+
+dq = deque([R])
+visit[R] = 1
+cnt = 2
+while dq :
+    R = dq.popleft()
+    for i in graph[R] :
+        if visit[i] == 0 :
+            dq.append(i)
+            visit[i] = cnt
+            cnt += 1
+
+print(*visit[1 : ], sep = '\n')
