80255e1fe379d38d9a1be2deb74ba9871ef70e4e
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2022-12-23 13:01:37 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 700 ms, Memory: 65140 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+
+def dfs(v):
+    visited[v] = True
+    for e in graph[v]:
+        if not visited[e]:
+            dfs(e)
+            
+N, M = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+visited = [False] * (N + 1)
+count = 0
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+for j in range(1, N + 1):
+    if not visited[j]:
+        dfs(j)
+        count += 1
+
+print(count)
