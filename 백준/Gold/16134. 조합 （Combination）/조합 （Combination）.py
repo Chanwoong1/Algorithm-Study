5b16b2e8d90807f5204263eb4c38bb3817084f14
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-01-18 15:04:43 +0900

    [Gold I] Title: 조합 (Combination), Time: 36 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.py
+++ 백준/Gold/16134. 조합 （Combination）/조합 （Combination）.py
@@ -1,6 +1,3 @@
-N, R = map(int, input().split())
-dp = [1] + [i for i in range(1, 1000001)]
-dp2 = [1] * 1000001
-for i in range(1, 1000001) :
-    dp2[i] = dp[i] * dp2[i - 1] % 1000000007
-print(int(dp2[N] / (dp2[N - R] * dp2[R])))
+from math import factorial
+n, k = map(int, input().split())
+print(int(factorial(n) / (factorial(k) * factorial(n-k)) % 1000000007))
