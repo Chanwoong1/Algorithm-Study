88532c364022f71da18566f2c992dc67261a9e15
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-02-12 15:46:29 +0900

    [Platinum V] Title: 마법의 구슬, Time: 848 ms, Memory: 117064 KB -BaekjoonHub

--- 백준/Platinum/1095. 마법의 구슬/마법의 구슬.py
+++ 백준/Platinum/1095. 마법의 구슬/마법의 구슬.py
@@ -1,4 +1,7 @@
-def ps(n, r) :
+# 마법의 구슬
+# https://www.acmicpc.net/problem/1095
+
+def ps(n, r) :  # n 팩토리얼 내부에 몇개의 r이 존재하는지 탐색
     tmp = r
     cnt = 0
     while n >= tmp :
@@ -7,14 +10,14 @@ def ps(n, r) :
     return cnt
 
 def check():
-    for num in range(M, 0, -1):
-        temp = num
+    for i in range(M, 0, -1):
+        tmp = i
         index = 0
         cnt = 0
         flag = True
-        while temp > 1:
-            if temp % prime_lst[index] == 0:
-                temp //= prime_lst[index]
+        while tmp > 1:
+            if tmp % prime_lst[index] == 0:
+                tmp //= prime_lst[index]
                 cnt += 1
                 if cnt > arr[index]:
                     flag = False
@@ -23,7 +26,7 @@ def check():
                 cnt = 0
                 index += 1
         if flag:
-            return num
+            return i
     return -1
 
 S, F, M = map(int, input().split())
@@ -35,6 +38,6 @@ for i in range(int(M ** 0.5) + 1) :
         for j in range(2 * i, M + 1 , i) :
             prime[j] = False
 prime_lst = [i for i in range(M + 1) if prime[i]]
-for i in range(len(prime_lst)) :
+for i in range(len(prime_lst)) :    # 조합에 포함된 소수의 갯수 구하기
     arr[i] = ps(S + F, prime_lst[i]) - ps(F, prime_lst[i]) - ps(S, prime_lst[i])
 print(check())
