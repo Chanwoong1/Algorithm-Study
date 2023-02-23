c70df4e5b45786ad4b7243527d1a1d41991c2d03
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-02-23 15:15:34 +0900

    [Gold IV] Title: 출근 경로, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Gold/5569. 출근 경로/출근 경로.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(false); cin.tie(0), cout.tie(0)
+#define ll long long
+
+// 0 : 꺾을 수 있음. 1 : 꺾을 수 없음
+int dp[110][110][2][2]; //dir, 1 or over2
+int ans = 0, w, h, mod = 100000;
+
+int main(){
+    fast;
+    cin >> w >> h;
+
+    for (int i = 2; i <= w; i++) dp[i][1][0][0] = 1;
+    for (int i = 2; i <= h; i++) dp[1][i][1][0] = 1;
+    for (int i = 2; i <= w; i++) {
+        for (int j = 2; j <= h; j++) {
+            dp[i][j][0][0] = (dp[i-1][j][0][0] + dp[i-1][j][0][1]) % mod;
+            dp[i][j][0][1] = dp[i-1][j][1][0];
+            dp[i][j][1][0] = (dp[i][j-1][1][0] + dp[i][j-1][1][1]) % mod;
+            dp[i][j][1][1] = dp[i][j-1][0][0];
+        }
+    }
+    for (int i = 0; i < 2; i++) for (int j = 0; j < 2; j++) ans += dp[w][h][i][j];
+    cout << ans % mod;
+}
