dd756bee98dd791fb035c941cdb281f7682b53e0
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-02-24 13:21:20 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    stringstream ss(s);
+    string num;
+    vector <long long> v;
+    while (getline(ss, num, ' ')) v.push_back(stoi(num));
+    sort(v.begin(), v.end());
+    answer += to_string(v[0]);
+    answer += " ";
+    answer += to_string(v[v.size() - 1]);
+    return answer;
+}
