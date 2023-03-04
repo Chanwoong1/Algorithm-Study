30796fca465e87e83ae48a238720c872fa509958
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-03-04 15:49:05 +0900

    [Bronze V] Title: Larger Sport Facility, Time: 272 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/16099. Larger Sport Facility/Larger Sport Facility.py
@@ -0,0 +1,5 @@
+for _ in range(int(input())) :
+    lt, wt, le, we = map(int, input().split())
+    if lt * wt > le * we : print("TelecomParisTech")
+    elif lt * wt < le * we : print("Eurecom")
+    else : print("Tie")
