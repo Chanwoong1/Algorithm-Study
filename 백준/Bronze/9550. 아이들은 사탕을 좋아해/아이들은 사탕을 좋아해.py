c7c901f2472f74f86aecbe66a0ed2518e0e4ca1d
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-03-26 20:58:59 +0900

    [Bronze III] Title: 아이들은 사탕을 좋아해, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9550. 아이들은 사탕을 좋아해/아이들은 사탕을 좋아해.py
+++ 백준/Bronze/9550. 아이들은 사탕을 좋아해/아이들은 사탕을 좋아해.py
@@ -1,5 +1,4 @@
 for _ in range(int(input())) :
     N, K = map(int, input().split())
     candy = [*map(int, input().split())]
-    answer = sum([i // K for i in candy])
-    print(answer)
+    print(sum([i // K for i in candy]))
