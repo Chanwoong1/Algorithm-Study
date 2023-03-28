57ee5ace42e9c20b5c9cf048bdc74c0cd6fadbe3
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-03-28 12:26:37 +0900

    [Bronze III] Title: Rectangles, Time: 128 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9950. Rectangles/Rectangles.py
@@ -0,0 +1,6 @@
+while True :
+    a, b, c = map(int, input().split())
+    if a == b == c == 0 : break
+    if a == 0 : print(c // b, b, c)
+    elif b == 0 : print(a, c // a, c)
+    else : print(a, b, a * b)
