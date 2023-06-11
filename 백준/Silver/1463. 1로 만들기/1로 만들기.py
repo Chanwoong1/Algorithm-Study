61f6869d7fbb271da4a1a2c0f6b1b782bd3295ab
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-06-11 10:04:11 +0900

    [Silver III] Title: 1로 만들기, Time: 680 ms, Memory: 39108 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,9 @@
+X = int(input())
+dp = [0, 0, 1, 1]
+for i in range(4, X + 1):
+    dp.append(dp[i - 1] + 1)
+    if i % 2 == 0 :
+        dp[i] = min(dp[i // 2] + 1, dp[i])
+    if i % 3 == 0 :
+        dp[i] = min(dp[i // 3] + 1, dp[i]) 
+print(dp[X])
