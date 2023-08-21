566fc4f61cfbea1ee70b34c1840a1f2f5577a451
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2023-08-21 19:37:59 +0900

    [Bronze III] Title: 행운의 편지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13311. 행운의 편지/행운의 편지.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0), cout.tie(0)
+#define ll long long
+
+int main()
+{
+    fast;
+    cout << -1;
+}
