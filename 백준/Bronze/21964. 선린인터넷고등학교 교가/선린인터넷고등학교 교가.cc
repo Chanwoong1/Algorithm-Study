98278303fa3799d3bc371fc79addfd375eff9341
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2024-02-21 09:58:28 +0900

    [Bronze IV] Title: 선린인터넷고등학교 교가, Time: 0 ms, Memory: 2392 KB -BaekjoonHub

---
+++ 백준/Bronze/21964. 선린인터넷고등학교 교가/선린인터넷고등학교 교가.cc
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0), cout.tie(0)
+#define ll long long
+
+int main() {
+  fast;
+  int N;
+  string s;
+  cin >> N >> s;
+  cout << s.substr(N - 5, 5);
+}
