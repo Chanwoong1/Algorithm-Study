dd6ad2da94edd7c2da0bfee511ea13771b4553c5
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2024-02-22 12:19:41 +0900

    [Bronze IV] Title: ЛОВНО КУЧЕ, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31282. ЛОВНО КУЧЕ/ЛОВНО КУЧЕ.cc
@@ -0,0 +1,21 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0), cout.tie(0)
+#define ll long long
+
+int main() {
+  fast;
+  int N, M, K, r;
+  cin >> N >> M >> K;
+  r = N / (K - M);
+  if (N % (K - M) != 0) r++;
+  cout << r;
+}
