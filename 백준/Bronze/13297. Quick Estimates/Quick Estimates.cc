9cd11a6b7775295f9f30162c961ce9a775ab5cd5
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2024-02-24 15:44:42 +0900

    [Bronze III] Title: Quick Estimates, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/13297. Quick Estimates/Quick Estimates.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0), cout.tie(0)
+#define ll long long
+
+int main() {
+  fast;
+  int N;
+  cin >> N;
+  string s;
+  while (N--) {
+    cin >> s;
+    cout << s.size() << "\n";
+  }
+}
