cbbe1e12643456f5a2d328262016db9cfa927224
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2024-03-19 08:05:03 +0900

    [Bronze V] Title: 分 (Minutes), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0), cout.tie(0)
+#define ll long long
+
+int main() {
+  fast;
+  int H, M;
+  cin >> H >> M;
+  cout << H * 60 + M;
+}
