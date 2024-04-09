2e192e88a30a19b73b245877b0c1cff0676a4d5c
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2024-04-09 12:23:22 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0), cout.tie(0)
+#define ll long long
+
+int main() {
+  fast;
+  ll N;
+  cin >> N;
+  cout << (N % 2 ? "evoLmoCicS" : "SciComLove");
+}
