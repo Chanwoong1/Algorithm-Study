9dd1a9bbbd2c477b11b9bdf5a3d55bb308a5db32
Chanwoong Jeong <84218652+chanwoong1@users.noreply.github.com>
2024-04-11 12:22:48 +0900

    [Bronze IV] Title: 三連続 (Three Consecutive), Time: 0 ms, Memory: 2392 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31636. 三連続 （Three Consecutive）/三連続 （Three Consecutive）.cc
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+#define fast                        \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0), cout.tie(0)
+#define ll long long
+
+int main() {
+  fast;
+  int N;
+  string s;
+  cin >> N >> s;
+  cout << (s.find("ooo") != s.npos ? "Yes" : "No");
+}
